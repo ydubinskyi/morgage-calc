e06d6272ce37e1e13a085d838483f738de638ce0
Yaroslav Dubinskyi <slavabilaz@gmail.com>
2023-09-15 21:42:36 +0200

    add base mortgage calc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
-import Image from "next/image";
-
 import { MortgageCalculator } from "@/components/MortgageCalculator/MortgageCalculator";
 
 export default function Home() {
-  return (
-    <div>
-      <MortgageCalculator />
-    </div>
-  );
+  return <MortgageCalculator />;
 }
